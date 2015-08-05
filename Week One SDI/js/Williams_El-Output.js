896292e2db2a348e9f33256af850ebca38050e77
Erica Williams <elwilliams2@fullsail.edu>
2015-08-05 04:32:11 -0400

    3rd committ

--- Week One SDI/js/Williams_El-Output.js
+++ Week One SDI/js/Williams_El-Output.js
@@ -10,3 +10,16 @@ console.log ("A good photograph is more than a focal point, but rather angles, d
 
 var groupPhotos = 5;//define a good number for a group of photos//
 console.log (groupPhotos);//print to console.
+
+var pointAndShoot= false;
+console.log (pointAndShoot);
+
+var interchangableLense = true;
+console.log (interchangableLense);
+
+var bestCamera =prompt ("Which camera is better? A point and shot or Interchangale Lense? ")
+console.log ("Which camera is better? A point and shot or Interchangale Lense?")//print prompt statement
+console.log (bestCamera);//print user response
+
+///End of code///
+
