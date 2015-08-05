387ec938767768fe110a2c2fd2089f28fe9a8c6f
Erica Williams <elwilliams2@fullsail.edu>
2015-08-05 04:22:03 -0400

    2nd committ
    string/number variable

--- Week One SDI/js/Williams_El-Output.js
+++ Week One SDI/js/Williams_El-Output.js
@@ -2,4 +2,11 @@
  * Created by EWilliams on 8/5/15.
  */
 // Assignment 1 Output//
-//SDI 1508 8/5/2015
+//SDI 1508 8/5/2015//
+//Variable output based on photography//
+
+var goodPhotograph = ("A good photograph is more than a focal point, but rather angles, depth, and colors");//String variable defined//
+console.log ("A good photograph is more than a focal point, but rather angles, depth, and colors");// print to console
+
+var groupPhotos = 5;//define a good number for a group of photos//
+console.log (groupPhotos);//print to console.
