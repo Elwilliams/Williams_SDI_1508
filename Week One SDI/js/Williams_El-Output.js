240531500f84c65082f45ab2b6774620aa749ae0
Erica Williams <elwilliams2@fullsail.edu>
2015-08-05 04:48:32 -0400

    Almost done
    polishing

--- Week One SDI/js/Williams_El-Output.js
+++ Week One SDI/js/Williams_El-Output.js
@@ -5,21 +5,24 @@
 //SDI 1508 8/5/2015//
 //Variable output based on photography//
 
-var goodPhotograph = ("A good photograph is more than a focal point, but rather angles, depth, and colors");//String variable defined//
-console.log ("A good photograph is more than a focal point, but rather angles, depth, and colors");// print to console
+var goodPhotograph = ("A good photograph is more than a focal point, but rather angles, depth, and colors.");//String variable defined//
+console.log ("A good photograph is more than a focal point, but rather angles, depth, and colors.");// print string variable to console//
+console.log (goodPhotograph + " It also is a matter of patience");//concatenated statement with string variable for context//
 
-var groupPhotos = 5;//define a good number for a group of photos//
-console.log (groupPhotos);//print to console.
+var groupPhotos = 5;//define a number variable, using a good number of photos for a group.//
+console.log (groupPhotos);//print variable only
+console.log ("A popular number of photos for a group is " + groupPhotos);//concatenated number variable for context
 
-var pointAndShoot= false;
-console.log (pointAndShoot);
+var pointAndShoot= false;// boolean variable defined
+console.log (pointAndShoot);// print a false boolean variable
 
-var interchangableLense = true;
-console.log (interchangableLense);
+var interchangableLense = true;//boolean variable defined
+console.log (interchangableLense);//print a true boolean variable
 
-var bestCamera =prompt ("Which camera is better? A point and shot or Interchangale Lense? ")
-console.log ("Which camera is better? A point and shot or Interchangale Lense?")//print prompt statement
+var bestCamera =prompt ("Which type of Camera, do you perfer?  ");//define bestCamera as a user prompted answer
+console.log ("Which type of Camera, do you perfer?");//print prompt statement
 console.log (bestCamera);//print user response
+console.log ("I prefer a Canon Rebel with 3 lenses when i take photos, but " + bestCamera + " is a good choice as well."); //concatenated final statement for context//
 
 ///End of code///
 
