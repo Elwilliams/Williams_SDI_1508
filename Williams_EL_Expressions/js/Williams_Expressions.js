c77a6d322dc22a646cd512eb8615953a169c2997
Erica Williams <elwilliams2@fullsail.edu>
2015-08-13 09:39:23 -0400

    Final Committ
    all set

--- Williams_EL_Expressions/js/Williams_Expressions.js
+++ Williams_EL_Expressions/js/Williams_Expressions.js
@@ -11,27 +11,27 @@
 console.log ("This is a hard value test formula for the area of a Trapezoid");
 var b1 = 10; //define variable b1
 var b2 = 12; //define variable b2
-console.log ("The first side of the trapezoid is" + b1);//print to console var b1 value
-console.log ("The first side of the trapezoid is" + b2);//print to console var b2 value
+console.log ("The first side of the trapezoid is " + b1);//print to console var b1 value
+console.log ("The second side of the trapezoid is " + b2);//print to console var b2 value
 
 var baseArray = [b1, b2];//define array
-console.log (baseArray);//print to console the array
+console.log (baseArray);//print to console the array variables to check accuracy
 
 //begin assignment operator example//
 var trapSides =3; //define variable and value//
-trapSides ++; //addition assignment operator//
+trapSides ++; //addition operator//
 console.log (trapSides); //print to console addition assignment operator//
-trapSides --; //subtraction assignment operator//
-console.log (trapSides); //print to concole subtraction assignmnet operator//
+trapSides --; //subtraction operator//
+console.log (trapSides); //print to concole subtraction assignment operator//
 //end assignment operator example//
 
-var total = baseArray [0] + baseArray[1];
-console. log (total);
+var total = baseArray [0] + baseArray[1];// addition w/ array using index
+console. log (total);// print to console the addition of array w/ index
 
 var h = 14;//define variable h
-console.log (h);//print to console variable h
+console.log ("The height is " + h);//print to console variable h
 
-area = (total/2)*h;//definition of formula
+var area = (total/2)*h;//definition of formula
 console.log ("The area of a trapezoid with bases of 10 & 12 and a height of 14 is " + area); //print to console
 //This is a working formula for the area of a trapezoid for edge 1 10, edge 2 12, height 10//
 
