5d72f4b595ef1729e493ff22a904bee3d393c49f
Erica Williams <elwilliams2@fullsail.edu>
2015-08-13 08:14:37 -0400

    another committ
    fine tuning to assignment

--- Williams_EL_Expressions/js/Williams_Expressions.js
+++ Williams_EL_Expressions/js/Williams_Expressions.js
@@ -7,25 +7,26 @@
 // Area of a Trapezoid is (b1+b2)/2 times h
 
 //Sample hard value trapezoid formula//
-    //Thia formula includes assignment operator example//
+    //Thia formula includes assignment operator example and array//
 console.log ("This is a hard value test formula for the area of a Trapezoid");
 var b1 = 10; //define variable b1
 var b2 = 12; //define variable b2
-console.log (b1);//print to console var b1 value
-console.log (b2);//print to console var b2 value
+console.log ("The first side of the trapezoid is" + b1);//print to console var b1 value
+console.log ("The first side of the trapezoid is" + b2);//print to console var b2 value
 
 var baseArray = [b1, b2];//define array
 console.log (baseArray);//print to console the array
 
 //begin assignment operator example//
 var trapSides =3; //define variable and value//
-trapSides += 0; //addition assignment operator//
+trapSides ++; //addition assignment operator//
 console.log (trapSides); //print to console addition assignment operator//
-trapSides -= 1; //subtraction assignment operator//
+trapSides --; //subtraction assignment operator//
 console.log (trapSides); //print to concole subtraction assignmnet operator//
+//end assignment operator example//
 
-var total= b1 + b2; //addition step 1 in formula
-console.log (total); //print to console
+var total = baseArray [0] + baseArray[1];
+console. log (total);
 
 var h = 14;//define variable h
 console.log (h);//print to console variable h
@@ -34,7 +35,6 @@ area = (total/2)*h;//definition of formula
 console.log ("The area of a trapezoid with bases of 10 & 12 and a height of 14 is " + area); //print to console
 //This is a working formula for the area of a trapezoid for edge 1 10, edge 2 12, height 10//
 
-//Assignmnet begins//
 //User Prompted Area of a Trapezoid//
 
 alert("To find the area of a trapezoid \nPlease Enter Base Edge 1, Base Edge 2, and the height in the next three windows"); //alert the user
@@ -52,14 +52,14 @@ var userH = prompt ("Please Enter Height"); // prompt 3 for the user to get an a
 console.log ("Please Enter Height");//print to console
 console.log (userH);//print to console
 
-var userTotal = Number(userB1)+ Number(userB2);//step 1 in formula for prompts
+var userTotal = Number(userB1) + Number(userB2);//step 1 in formula for prompts
 console.log (userTotal);//print to console
 
 var userArea = (userTotal)/2 * userH; //solution prompt for user
 console.log (userArea);//print to console
 
 alert ("The area of your trapezoid is " + userArea);//show user the area of the trapezoid
-console.log ("The area of the trapezoid is " + userArea);
+console.log ("The area of the trapezoid is " + userArea);// print out area of the trapezoid
 
 //End of the calculator for a trapezoid//
 
