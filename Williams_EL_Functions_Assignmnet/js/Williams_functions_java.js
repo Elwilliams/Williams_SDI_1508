ad64a94e87e1176c377d43fc3b6da2f74d821cc3
Erica Williams <elwilliams2@fullsail.edu>
2015-08-23 19:16:37 -0400

    First Committ
    Getting files set up and begin java

---
+++ Williams_EL_Functions_Assignmnet/js/Williams_functions_java.js
@@ -0,0 +1,19 @@
+/**
+ * Created by EWilliams on 8/23/15.
+ */
+//Erica Williams Functions Assignment//
+// Week 4 SDI 1508//
+
+//The Powerball has (5) random numbers btwn 1 and 59 plus (1) Powerball btwn 1 and 35//
+//The Florida Lottery has 6 random numbers btwn 1 and 53//
+//No numbers may repeat for either//
+
+//Start Main code//
+
+var lotteryType = prompt("Which lottery results do you want? \nPlease enter either Powerball or Florida Lottery");
+//establish the variable lotteryType and users response.
+console.log ("Which lottery results do you want? \nPlease enter either Powerball or Florida Lottery");//print user prompt to the console.
+console.log("The user entered " + lotteryType);//print to the console the user's response with context string.
+
+//Stop main code//
+
